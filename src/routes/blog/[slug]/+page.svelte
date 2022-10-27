33ca391f8effe695e7a7e6bb5682de75d12369cf
Shinichi Okada <okada.shin@gmail.com>
2022-10-27 11:49:05 +0900

    feat: add blog dir

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	// src/routes/blog/[slug]/+page.svelte
+	import type { PageData } from './$types';
+	export let data: PageData;
+</script>
+
+<svelte:component this={data.content} />
